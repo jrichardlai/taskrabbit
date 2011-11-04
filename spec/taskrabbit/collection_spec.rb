41887644c403828423ab89d5d2fd15584382134b
jrichardlai <jrichardlai@gmail.com>
2011-11-04 16:41:15 -0700

    Added some test for Collection.

--- spec/taskrabbit/collection_spec.rb
+++ spec/taskrabbit/collection_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe Taskrabbit::Collection do
+  subject { Taskrabbit::Collection.new }
+
+  Taskrabbit::Collection::ARRAY_METHODS.each do |method|
+    it "should define #{method}" do
+      subject.should_receive(method)
+      expect { subject.send(method) }.to_not raise_error
+    end
+    
+    it "should call the #{method} on items" do
+      subject.items.should_receive(method)
+      expect { subject.send(method) }.to_not raise_error
+    end
+  end
 end
